60ea4679c5f2a4a5582974dd4ecd225c73a99261
WackJeston <33kcaj33@gmail.com>
2021-10-08 15:53:13 +0100

    Finished Mobile Menu and Most of Cookie Pop Up is done

--- js/pop-up-menu.js
+++ js/pop-up-menu.js
@@ -1,19 +1,28 @@
-
-
 const menuButton = document.querySelector(".menu-but")
 const page = document.querySelector(".page-container")
+const pageCover = document.querySelector(".page-cover")
+const line1 = document.querySelector("#line-1")
+const line2 = document.querySelector("#line-2")
+const line3 = document.querySelector("#line-3")
 let active = false
 
-if (active === false) {
-  menuButton.addEventListener("click", () => {
-    body.style.transform = "translateX(-260px)"
-    active = true
-  })
-}
 
-if (active === true) {
-  page.addEventListener("click", () => {
-    body.style.transform = "translateX(0px)"
+menuButton.addEventListener("click", () => {
+  if (active === false) {
+    body.classList.remove("menu-non-active")
+    body.classList.add("menu-active")
+    line1.style.animation = "line-key-1-f 450ms forwards"
+    line2.style.animation = "line-key-2-f 450ms forwards"
+    line3.style.animation = "line-key-3-f 450ms forwards"
+    pageCover.style.display = "block"
+    active = true
+  } else if (active === true) {
+    body.classList.remove("menu-active")
+    body.classList.add("menu-non-active")
+    line1.style.animation = "line-key-1-b 450ms forwards"
+    line2.style.animation = "line-key-2-b 450ms forwards"
+    line3.style.animation = "line-key-3-b 450ms forwards"
+    pageCover.style.display = "none"
     active = false
-  })
-}
+  }
+})
