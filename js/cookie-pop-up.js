c734e1e4346a6eb83490210774fc38fa84462861
WackJeston <33kcaj33@gmail.com>
2021-10-11 16:12:16 +0100

    Finished cookie CSS

---
+++ js/cookie-pop-up.js
@@ -0,0 +1,5 @@
+const popUpHeight = document.querySelector(".cookie-pop-up").offsetHeight
+const containerHeight = document.querySelector(".cookie-container").offsetHeight
+const container = document.querySelector(".cookie-container")
+const acceptCookies = document.querySelector(".cookie-ac")
+let activeCookie = false
--- js/pop-up-menu.js
+++ js/pop-up-menu.js
@@ -4,25 +4,25 @@ const pageCover = document.querySelector(".page-cover")
 const line1 = document.querySelector("#line-1")
 const line2 = document.querySelector("#line-2")
 const line3 = document.querySelector("#line-3")
-let active = false
+let activeMenu = false
 
 
 menuButton.addEventListener("click", () => {
-  if (active === false) {
+  if (activeMenu === false) {
     body.classList.remove("menu-non-active")
     body.classList.add("menu-active")
     line1.style.animation = "line-key-1-f 450ms forwards"
     line2.style.animation = "line-key-2-f 450ms forwards"
     line3.style.animation = "line-key-3-f 450ms forwards"
     pageCover.style.display = "block"
-    active = true
-  } else if (active === true) {
+    activeMenu = true
+  } else if (activeMenu === true) {
     body.classList.remove("menu-active")
     body.classList.add("menu-non-active")
     line1.style.animation = "line-key-1-b 450ms forwards"
     line2.style.animation = "line-key-2-b 450ms forwards"
     line3.style.animation = "line-key-3-b 450ms forwards"
     pageCover.style.display = "none"
-    active = false
+    activeMenu = false
   }
 })
