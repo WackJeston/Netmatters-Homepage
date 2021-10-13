4bc17bbbf66f0e880158ccdeb4c7ff30c18a241f
WackJeston <33kcaj33@gmail.com>
2021-10-13 08:30:23 +0100

    Been told to move on :(

--- js/cookie-pop-up.js
+++ js/cookie-pop-up.js
@@ -1,6 +1,9 @@
 const container = document.querySelector(".cookie-container")
+const settings = document.querySelector(".cookie-settings")
 const changeSettings = document.querySelector(".cookie-cs")
 const acceptCookies = document.querySelector(".cookie-ac")
+const cancel = document.querySelector(".cancel-cs")
+const accept = document.querySelector(".accept-cs")
 
 $(document).ready(function() {
   let counter = localStorage.getItem('counter');
@@ -12,4 +15,16 @@ $(document).ready(function() {
 
 acceptCookies.addEventListener("click", function() {
   container.style.display = "none"
+})
+
+changeSettings.addEventListener("click", function() {
+  settings.style.display = "block"
+})
+
+cancel.addEventListener("click", function() {
+  settings.style.display = "none"
+})
+
+accept.addEventListener("click", function() {
+  settings.style.display = "none"
 })
